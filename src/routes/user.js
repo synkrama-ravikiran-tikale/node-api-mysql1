8f1de5f2407132c196b7c28a5f0c627166b8f338
Ravikiran Tikale <ravikiran@synkrama.com>
2024-07-08 14:57:31 +0530

    Updated .gitignore to ignore 'uploads' directory and modified multer storage destination in user.js route

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,7 +10,7 @@ const path = require("path");
 // Configure multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Specify the upload directory
+    cb(null, "uploads"); // Specify the upload directory
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
